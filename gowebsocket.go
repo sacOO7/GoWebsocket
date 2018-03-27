addfc45039f3ef3e6ea4512b17c3d39e001e55a0
sachin <sachinshinde7676@gmail.com>
2018-03-27 19:21:39 +0530

    Refactored code, made memebers of socket public

--- gowebsocket.go
+++ gowebsocket.go
@@ -13,11 +13,11 @@ import (
 )
 
 type Socket struct {
-	conn              *websocket.Conn
-	websocketDialer   *websocket.Dialer
-	url               string
-	connectionOptions ConnectionOptions
-	requestHeader     http.Header
+	Conn              *websocket.Conn
+	WebsocketDialer   *websocket.Dialer
+	Url               string
+	ConnectionOptions ConnectionOptions
+	RequestHeader     http.Header
 	OnConnected       func(socket Socket)
 	OnTextMessage     func(message string, socket Socket)
 	OnBinaryMessage   func(data [] byte, socket Socket)
@@ -25,55 +25,49 @@ type Socket struct {
 	OnDisconnected    func(err error, socket Socket)
 	OnPingReceived    func(data string, socket Socket)
 	OnPongReceived    func(data string, socket Socket)
-	isConnected       bool
+	IsConnected       bool
 	sendMu            *sync.Mutex // Prevent "concurrent write to websocket connection"
 	receiveMu         *sync.Mutex
 }
 
 type ConnectionOptions struct {
 	useCompression bool
-	reconnect      bool
 	useSSL         bool
 	proxy          func(*http.Request) (*url.URL, error)
 	subprotocols   [] string
 }
 
-func New(url string, requestHeader http.Header) Socket {
-	return Socket{url: url, requestHeader: requestHeader,
-		connectionOptions: ConnectionOptions{
-			reconnect:      false,
+// todo Yet to be done
+type ReconnectionOptions struct {
+}
+
+func New(url string) Socket {
+	return Socket{Url: url,
+		ConnectionOptions: ConnectionOptions{
 			useCompression: false,
 			useSSL:         true,
 		},
-		websocketDialer: &websocket.Dialer{},
+		WebsocketDialer: &websocket.Dialer{},
 		sendMu: &sync.Mutex{},
 		receiveMu: &sync.Mutex{},
 	}
 }
 
-func (socket *Socket) setConnectionOptions(options ConnectionOptions) {
-	socket.connectionOptions = options
-}
-
-func (socket *Socket) IsConnected() bool {
-	return socket.isConnected
-}
-
-func (socket *Socket) registerConnectionOptions() {
-	socket.websocketDialer.EnableCompression = socket.connectionOptions.useCompression
-	socket.websocketDialer.TLSClientConfig = &tls.Config{InsecureSkipVerify: socket.connectionOptions.useSSL}
-	socket.websocketDialer.Proxy = socket.connectionOptions.proxy
-	socket.websocketDialer.Subprotocols = socket.connectionOptions.subprotocols
+func (socket *Socket) setConnectionOptions() {
+	socket.WebsocketDialer.EnableCompression = socket.ConnectionOptions.useCompression
+	socket.WebsocketDialer.TLSClientConfig = &tls.Config{InsecureSkipVerify: socket.ConnectionOptions.useSSL}
+	socket.WebsocketDialer.Proxy = socket.ConnectionOptions.proxy
+	socket.WebsocketDialer.Subprotocols = socket.ConnectionOptions.subprotocols
 }
 
 func (socket *Socket) Connect() {
 	var err error;
-	socket.registerConnectionOptions()
+	socket.setConnectionOptions()
 
-	socket.conn, _, err = socket.websocketDialer.Dial(socket.url, socket.requestHeader)
+	socket.Conn, _, err = socket.WebsocketDialer.Dial(socket.Url, socket.RequestHeader)
 
 	if err != nil {
-		socket.isConnected = false
+		socket.IsConnected = false
 		if socket.OnConnectError != nil {
 			socket.OnConnectError(err, *socket)
 		}
@@ -81,31 +75,31 @@ func (socket *Socket) Connect() {
 	}
 
 	if socket.OnConnected != nil {
-		socket.isConnected = true
+		socket.IsConnected = true
 		socket.OnConnected(*socket)
 	}
 
-	defaultPingHandler := socket.conn.PingHandler()
-	socket.conn.SetPingHandler(func(appData string) error {
+	defaultPingHandler := socket.Conn.PingHandler()
+	socket.Conn.SetPingHandler(func(appData string) error {
 		if socket.OnPingReceived != nil {
 			socket.OnPingReceived(appData, *socket)
 		}
 		return defaultPingHandler(appData)
 	})
 
-	defaultPongHandler := socket.conn.PongHandler()
-	socket.conn.SetPongHandler(func(appData string) error {
+	defaultPongHandler := socket.Conn.PongHandler()
+	socket.Conn.SetPongHandler(func(appData string) error {
 		if socket.OnPongReceived != nil {
 			socket.OnPongReceived(appData, *socket)
 		}
 		return defaultPongHandler(appData)
 	})
 
-	defaultCloseHandler := socket.conn.CloseHandler()
-	socket.conn.SetCloseHandler(func(code int, text string) error {
+	defaultCloseHandler := socket.Conn.CloseHandler()
+	socket.Conn.SetCloseHandler(func(code int, text string) error {
 		result := defaultCloseHandler(code, text)
 		if socket.OnDisconnected != nil {
-			socket.isConnected = false
+			socket.IsConnected = false
 			socket.OnDisconnected(errors.New(text), *socket)
 		}
 		return result
@@ -114,7 +108,7 @@ func (socket *Socket) Connect() {
 	go func() {
 		for {
 			socket.receiveMu.Lock()
-			messageType, message, err := socket.conn.ReadMessage()
+			messageType, message, err := socket.Conn.ReadMessage()
 			socket.receiveMu.Unlock()
 			if err != nil {
 				log.Println("read:", err)
@@ -137,9 +131,7 @@ func (socket *Socket) Connect() {
 }
 
 func (socket *Socket) SendText(message string) {
-	socket.sendMu.Lock()
-	err := socket.conn.WriteMessage(websocket.TextMessage, [] byte (message))
-	socket.sendMu.Unlock()
+	err := socket.send(websocket.TextMessage, [] byte (message))
 	if err != nil {
 		log.Println("write:", err)
 		return
@@ -147,25 +139,28 @@ func (socket *Socket) SendText(message string) {
 }
 
 func (socket *Socket) SendBinary(data [] byte) {
-	socket.sendMu.Lock()
-	err := socket.conn.WriteMessage(websocket.BinaryMessage, data)
-	socket.sendMu.Unlock()
+	err := socket.send(websocket.BinaryMessage, data)
 	if err != nil {
 		log.Println("write:", err)
 		return
 	}
 }
 
-func (socket *Socket) Close() {
+func (socket *Socket) send(messageType int, data [] byte) error {
 	socket.sendMu.Lock()
-	err := socket.conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	err := socket.Conn.WriteMessage(messageType, data)
 	socket.sendMu.Unlock()
+	return err
+}
+
+func (socket *Socket) Close() {
+	err := socket.send(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 	if err != nil {
 		log.Println("write close:", err)
 	}
-	socket.conn.Close()
+	socket.Conn.Close()
 	if socket.OnDisconnected != nil {
-		socket.isConnected = false
+		socket.IsConnected = false
 		socket.OnDisconnected(err, *socket)
 	}
 }
@@ -174,7 +169,7 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	socket := New("ws://echo.websocket.org/", nil);
+	socket := New("ws://echo.websocket.org/");
 	socket.OnConnectError = func(err error, socket Socket) {
 		log.Fatal("Got connect error")
 	};
