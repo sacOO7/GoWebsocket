sacOO7/GoWebsocket

commit 02b66393aed7796be8ea6f719b394d48446ad685

add response status info in case of connection error

gowebsocket.go
@@ -76,12 +76,14 @@ func (socket *Socket) setConnectionOptions() {
 
 func (socket *Socket) Connect() {
 	var err error;
+	var resp *http.Response
 	socket.setConnectionOptions()
 
-	socket.Conn, _, err = socket.WebsocketDialer.Dial(socket.Url, socket.RequestHeader)
+	socket.Conn, resp, err = socket.WebsocketDialer.Dial(socket.Url, socket.RequestHeader)
 
 	if err != nil {
 		logger.Error.Println("Error while connecting to server ", err)
+		logger.Error.Println("HTTP Response %d status: %s", resp.StatusCode, resp.Status)
 		socket.IsConnected = false
 		if socket.OnConnectError != nil {
 			socket.OnConnectError(err, *socket)

commit b1ce6de0a138580b29cf823ade8da9930067dc86

Fix printing of response code
On some errors, the respoonse code is not set (nil). The commit
02b66393aed7796be8ea6f719b394d48446ad685 introduced printing the content
of the response code which might panic if the response code is nil.

gowebsocket.go
@@ -86,7 +86,9 @@ func (socket *Socket) Connect() {
 
 	if err != nil {
 		logger.Error.Println("Error while connecting to server ", err)
-		logger.Error.Println("HTTP Response %d status: %s", resp.StatusCode, resp.Status)
+		if resp != nil {
+			logger.Error.Println("HTTP Response %d status: %s", resp.StatusCode, resp.Status)
+		}
 		socket.IsConnected = false
 		if socket.OnConnectError != nil {
 			socket.OnConnectError(err, *socket)