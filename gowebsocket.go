843775b2fbd64a6d2b801dcfacf33a56f73411e4
Nero <0xnero@users.noreply.github.com>
2021-05-10 15:42:45 -0400

    gofmt

--- gowebsocket.go
+++ gowebsocket.go
@@ -1,14 +1,14 @@
 package gowebsocket
 
 import (
-	"github.com/gorilla/websocket"
-	"net/http"
-	"errors"
 	"crypto/tls"
-	"net/url"
-	"sync"
+	"errors"
+	"github.com/gorilla/websocket"
 	"github.com/sacOO7/go-logger"
+	"net/http"
+	"net/url"
 	"reflect"
+	"sync"
 	"time"
 )
 
@@ -22,7 +22,7 @@ func (socket Socket) EnableLogging() {
 }
 
 func (socket Socket) GetLogger() logging.Logger {
-	return logger;
+	return logger
 }
 
 type Socket struct {
@@ -33,7 +33,7 @@ type Socket struct {
 	RequestHeader     http.Header
 	OnConnected       func(socket Socket)
 	OnTextMessage     func(message string, socket Socket)
-	OnBinaryMessage   func(data [] byte, socket Socket)
+	OnBinaryMessage   func(data []byte, socket Socket)
 	OnConnectError    func(err error, socket Socket)
 	OnDisconnected    func(err error, socket Socket)
 	OnPingReceived    func(data string, socket Socket)
@@ -48,7 +48,7 @@ type ConnectionOptions struct {
 	UseCompression bool
 	UseSSL         bool
 	Proxy          func(*http.Request) (*url.URL, error)
-	Subprotocols   [] string
+	Subprotocols   []string
 }
 
 // todo Yet to be done
@@ -57,7 +57,7 @@ type ReconnectionOptions struct {
 
 func New(url string) Socket {
 	return Socket{
-		Url: url,
+		Url:           url,
 		RequestHeader: http.Header{},
 		ConnectionOptions: ConnectionOptions{
 			UseCompression: false,
@@ -78,7 +78,7 @@ func (socket *Socket) setConnectionOptions() {
 }
 
 func (socket *Socket) Connect() {
-	var err error;
+	var err error
 	var resp *http.Response
 	socket.setConnectionOptions()
 
@@ -163,14 +163,14 @@ func (socket *Socket) Connect() {
 }
 
 func (socket *Socket) SendText(message string) {
-	err := socket.send(websocket.TextMessage, [] byte (message))
+	err := socket.send(websocket.TextMessage, []byte(message))
 	if err != nil {
 		logger.Error.Println("write:", err)
 		return
 	}
 }
 
-func (socket *Socket) SendBinary(data [] byte) {
+func (socket *Socket) SendBinary(data []byte) {
 	err := socket.send(websocket.BinaryMessage, data)
 	if err != nil {
 		logger.Error.Println("write:", err)
@@ -178,7 +178,7 @@ func (socket *Socket) SendBinary(data [] byte) {
 	}
 }
 
-func (socket *Socket) send(messageType int, data [] byte) error {
+func (socket *Socket) send(messageType int, data []byte) error {
 	socket.sendMu.Lock()
 	err := socket.Conn.WriteMessage(messageType, data)
 	socket.sendMu.Unlock()
